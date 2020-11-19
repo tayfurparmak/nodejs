a827f8092855af77fa1f459d1c05cbdf31f17f8f
tayfurparmak <74604272+tayfurparmak@users.noreply.github.com>
2020-11-19 19:46:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const express = require('express');
-const app = express();
-var port = process.env.PORT || 8080;
-app.listen(port);
+//const express = require('express');
+//const app = express();
+//var port = process.env.PORT || 8080;
+//app.listen(port);
 
-app.use(express.static('public'))
+//app.use(express.static('public'))
  
 
+var express = require('express');
+var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port);
