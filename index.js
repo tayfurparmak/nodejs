95059923991dd1dd4a1f8db7df954d086f410b2d
tayfurparmak <74604272+tayfurparmak@users.noreply.github.com>
2020-11-19 19:30:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
 
-app.listen(process.env.PORT);
 app.use(express.static('public'))
  
 
